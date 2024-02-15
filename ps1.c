85abd425dac27d6a86e20ff16040037c3e9d3a2a
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-02-15 10:23:53 +0100

    1 done

--- ps1.c
+++ ps1.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main(){
-
-    double ans = 10.0 + 2.0/3.0 - 2.0*2.0;
+    /*
+    double ansF = 10.0 + 2.0/3.0 - 2.0*2.0;
+    double ans = 10.0 + 2.0/((3.0 - 2.0)*2.0);
+    printf("%.1f\n", ansF);
     printf("%.1f\n", ans);
     // I need ans = 11.0
+    */
 
     return 0;
 }
