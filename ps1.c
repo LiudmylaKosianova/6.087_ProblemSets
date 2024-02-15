ab6f33c7aadf876a30db5b65856c31e0852189a6
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-02-15 10:39:01 +0100

    2 done

--- ps1.c
+++ ps1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define squared(x) ((x)*(x))
+
 int main(){
     /*
     double ansF = 10.0 + 2.0/3.0 - 2.0*2.0;
@@ -8,6 +10,12 @@ int main(){
     printf("%.1f\n", ans);
     // I need ans = 11.0
     */
+   double ans = 180 / squared(2+1);
+   printf("%.1f\n", ans);
+   //ans = 93.0 when #define squared(x) x*x
+   //ans = 36.0 when #define squared(x) (x*x)
+   //ans = 180.0 when #define squared(x) (x)*(x)
+   //ans = 20.0 when #define squared(x) ((x)*(x))
 
     return 0;
 }
