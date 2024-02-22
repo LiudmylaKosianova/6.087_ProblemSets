fcfeb74e5ab9f0d4b50f1cefd02975b0b73fba48
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-02-22 08:40:13 +0100

    ps2 start

---
+++ ps2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+
+int main(){
+
+    printf("Size of char is %zu\n", sizeof(char));
+    printf("Minimum value of char is %d\n", CHAR_MIN);
+    printf("Maximum value of char is %d\n", CHAR_MAX);
+    
+
+    return 0;
+}
